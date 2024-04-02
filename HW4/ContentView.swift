9bc50c5ed6936e53aa48658f139dd48b0760ac85
Mintellect310 <40604693+Mintellect310@users.noreply.github.com>
2024-04-02 16:46:26 -0700

    Initial Commit

---
+++ HW4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW4
+//
+//  Created by Maheeth Reddy Maramreddy on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Maheeth Reddy Maramreddy on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
